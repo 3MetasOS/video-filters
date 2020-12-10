525d9a667cc45aef08dcf391fcc622cfc4f3f0d9
Juan Pelaez <juancarlospelaez@Juans-Mac-mini.local>
2020-12-09 21:59:55 -0700

    added webrtc and config for local ssl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoFilterComponent } from './pages/video-filter/video-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoFilterComponent
   ],
   imports: [
     BrowserModule,
