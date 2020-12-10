9daa678ebdc8088f2041746ee7113aba2e9d7234
Juan Pelaez <juan.pelaez@3metas.com>
2020-12-09 22:29:09 -0700

    added camera capture

--- src/app/pages/video-filter/video-filter.component.ts
+++ src/app/pages/video-filter/video-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-video-filter',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoFilterComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('me') me: any;
+
+  pc: any;
+  pcPeers: {
+    [k: string]: RTCPeerConnection;
+  };
+  localStream: any;
+
+  constructor() {
+    this.pcPeers = {};
+  }
 
   ngOnInit(): void {
   }
 
+  startVideo(): void {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: true })
+      .then((stream) => {
+        this.localStream = stream;
+        this.me.nativeElement.srcObject = stream;
+        this.me.nativeElement.muted = true;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
